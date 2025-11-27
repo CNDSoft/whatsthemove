678e9f32dd1198a83170ca6c618dddaff5462ea5
Cem Sertkaya <cem.sertkaya@cndsoftware.com>
2025-11-27 11:43:55 +0300

    feat: implement launch screen with storyboard and update LaunchScreenView to use images

--- WhatsTheMove/UI/LaunchScreen/LaunchScreenView.swift
+++ WhatsTheMove/UI/LaunchScreen/LaunchScreenView.swift
@@ -12,20 +12,14 @@ struct LaunchScreenView: View {
     
     var body: some View {
         ZStack {
-            Color(hex: "11104B")
+            Image("auth-background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
                 .ignoresSafeArea()
             
-            VStack(spacing: 16) {
-                Text("wtm")
-                    .font(.system(size: 92, weight: .black, design: .rounded))
-                    .italic()
-                    .foregroundColor(Color(hex: "F8F7F1"))
-                    .tracking(-1.84)
-                
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle(tint: Color(hex: "E7FF63")))
-                    .scaleEffect(1.5)
-            }
+            Image("wtm-logo")
+                .resizable()
+                .frame(width: 240, height: 240)
         }
     }
 }
